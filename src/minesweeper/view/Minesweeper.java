b1ae8c94e86bd6e6c65b8e89708d052b0a9e54a0
Rappee <wouter.rappe@hotmail.com>
2016-03-19 15:59:00 +0100

    Organized packages and created empty model class

--- src/minesweeper/view/Minesweeper.java
+++ src/minesweeper/view/Minesweeper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package minesweeper;
+package minesweeper.view;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -21,19 +21,8 @@ public class Minesweeper extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Hello World!");
