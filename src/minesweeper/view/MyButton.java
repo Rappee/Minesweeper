f37b493264e28d25aeff64485b27e2b0520009e0
Rappee <wouter.rappe@hotmail.com>
2016-03-19 18:09:01 +0100

    Created MyButton Class

---
+++ src/minesweeper/view/MyButton.java
@@ -0,0 +1,22 @@
+package minesweeper.view;
+
+public class MyButton {
+    private int row;
+    private int col;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+}
