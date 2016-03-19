79de2a9c53cdb181c1c668b88ad87df411eecfcb
Rappee <wouter.rappe@hotmail.com>
2016-03-19 16:17:58 +0100

    added Status enum and Tile class

---
+++ src/minesweeper/model/Status.java
@@ -0,0 +1,5 @@
+package minesweeper.model;
+
+public enum Status {
+    BOMB, CLICKED, EXPLODED, EMPTY
+}
---
+++ src/minesweeper/model/Tile.java
@@ -0,0 +1,42 @@
+package minesweeper.model;
+
+public class Tile {
+    private Status status;
+    private boolean marked;
+    private int bombCount;
+
+    public Tile(Status status) {
+        this.status = status;
+        this.marked = false;
+        this.bombCount = 0;
+    }
+    
+    // Getters and Setters
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public boolean isMarked() {
+        return marked;
+    }
+
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+
+    public int getBombCount() {
+        return bombCount;
+    }
+
+    public void setBombCount(int bombCount) {
+        this.bombCount = bombCount;
+    }
+    
+    
+    
+}
